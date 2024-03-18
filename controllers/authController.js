ef53835f674f657bad716b9c56ae216b976df6a8
spookyreaper <brandon.alvarado9991@gmail.com>
2024-03-18 09:30:31 -0400

    removed console.log

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,11 +9,9 @@ const getRegister = (req, res) => {
   res.render('register', { title: 'Register', errorMessage: null, role: '' });
 };
 
-
-
 const postLogin = async (req, res) => {
   try {
-    const { username, password } = req.body; 
+    const { username, password } = req.body;
 
     const user = await User.findOne({
       $or: [
@@ -35,29 +33,23 @@ const postLogin = async (req, res) => {
       return res.render('login', { title: 'Login', errorMessage: 'Invalid password' });
     }
   } catch (error) {
-    console.error('Login error:', error);
     res.render('login', { title: 'Login', errorMessage: 'An error occurred during login' });
   }
 };
 
-
 const postRegister = async (req, res) => {
   try {
     const { username, email, password, firstName, lastName, role } = req.body;
-    
-    console.log('Attempting to register user:', username); 
 
     const existingUser = await User.findOne({
       $or: [{ username: username.toLowerCase().trim() }, { email: email.toLowerCase().trim() }]
     });
 
     if (existingUser) {
-      console.log('Username or email already exists'); 
       return res.render('register', { title: 'Register', errorMessage: 'Username or email already exists', role });
     }
 
-       const hashedPassword = await bcrypt.hash(password, 12); 
-    console.log(`Registering user: ${username}, Hash: ${hashedPassword}`); 
+    const hashedPassword = await bcrypt.hash(password, 12);
 
     const newUser = await User.create({
       username: username.toLowerCase().trim(),
@@ -68,34 +60,25 @@ const postRegister = async (req, res) => {
       role,
     });
 
-    console.log('Registered new user:', newUser); 
-
     req.session.userId = newUser._id;
     req.session.role = newUser.role;
-
 
     const redirectUrl = newUser.role === 'owner' ? '/completeOwnerInfo' : '/sitter/complete-info';
     return res.redirect(redirectUrl);
   } catch (error) {
-    console.error('Registration error:', error);
     res.status(500).render('register', { title: 'Register', errorMessage: 'An error occurred during registration', role: '' });
   }
 };
 
-
 const logout = (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      console.error('Logout error:', err);
       return res.status(500).send('Could not log out, please try again.');
     }
-    res.clearCookie('connect.sid'); 
+    res.clearCookie('connect.sid');
     return res.redirect('/login');
   });
 };
-
-
-
 
 module.exports = {
   getLogin,
